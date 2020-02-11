e973621a756e46e2a60cffae7f729e32f5ae9b0e
Liempo <gonzalesalec@gmail.com>
2020-02-11 21:07:17 +0800

    Added furniture dialog

---
+++ app/src/main/java/com/liempo/furniture/FurnitureAdapter.kt
@@ -0,0 +1,60 @@
+package com.liempo.furniture
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_furniture.view.*
+
+class FurnitureAdapter(private val activity: MainActivity):
+    RecyclerView.Adapter<FurnitureAdapter.ViewHolder>() {
+
+    // List of model names (key)
+    private val items = arrayListOf<Furniture>()
+
+    internal fun addItem(key: String, uri: Uri) {
+        items.add(Furniture(key, uri))
+        notifyItemInserted(items.lastIndex)
+    }
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val preview: ImageView = view.preview
+        val name: TextView = view.model_name
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+            ViewHolder = ViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_furniture, parent, false))
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        val previewImageUri = Uri.parse(
+            "file:///android_asset/" +
+                    "previews/${item.key}.png")
+
+        Glide.with(holder.preview)
+            .load(previewImageUri)
+            .centerCrop()
+            .into(holder.preview)
+
+        holder.itemView.setOnClickListener {
+            activity.selectModel(item.uri)
+        }
+
+        @SuppressLint("DefaultLocale")
+        holder.name.text = item.key.capitalize()
+            .replace("_", " ")
+    }
+
+    data class Furniture(
+        val key: String,
+        val uri: Uri
+    )
+}
--- app/src/main/java/com/liempo/furniture/MainActivity.kt
+++ app/src/main/java/com/liempo/furniture/MainActivity.kt
@@ -1,12 +1,82 @@
 package com.liempo.furniture
 
+import android.app.ActivityManager
+import android.content.Context
+import android.net.Uri
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.ar.core.ArCoreApk
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 
 class MainActivity : AppCompatActivity() {
+
+    private var model: ModelRenderable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Check if device is support before initializing fragment
+        if (isDeviceSupport().not())
+            Toast.makeText(
+                this,
+                "OpenGL version must be above $MIN_OPENGL_VERSION.",
+                Toast.LENGTH_LONG
+            ).show()
+
+        // Get the instance of arFragment
+        val ux = supportFragmentManager
+            .findFragmentById(R.id.ux_fragment) as ArFragment
+
+        ux.setOnTapArPlaneListener { hit, _, _ ->
+            if (model == null) {
+                Toast.makeText(
+                    this,
+                    "No furniture selected.",
+                    Toast.LENGTH_LONG
+                ).show()
+                return@setOnTapArPlaneListener
+            }
+
+            // Create the anchor
+            val anchor = hit.createAnchor()
+            val node = AnchorNode(anchor).apply {
+                setParent(ux.arSceneView.scene)
+            }
+
+            // Create the transformable node
+            TransformableNode(ux.transformationSystem).apply {
+                setParent(node); renderable = model; select()
+                scaleController.minScale = 0.2f
+                scaleController.maxScale = 1.5f
+            }
+        }
+    }
+
+    internal fun selectModel(uri: Uri) {
+        ModelRenderable.builder()
+            .setSource(this, uri)
+            .build()
+            .thenAccept {
+                model = it
+            }.exceptionally { error(it) }
+    }
+
+    private fun getOpenGLVersion(): Double =
+        (getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+            .deviceConfigurationInfo.glEsVersion.toDouble()
+
+    private fun isDeviceSupport(): Boolean =
+        (ArCoreApk.getInstance().checkAvailability(this)
+                != ArCoreApk.Availability.UNSUPPORTED_DEVICE_NOT_CAPABLE) &&
+                (getOpenGLVersion() > MIN_OPENGL_VERSION)
+
+    companion object {
+        private const val MIN_OPENGL_VERSION = 3.0
     }
 }
