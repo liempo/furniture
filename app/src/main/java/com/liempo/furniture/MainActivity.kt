6e3b3ec7877cdf27769579cfb7a04ac487802423
Liempo <gonzalesalec@gmail.com>
2020-02-11 22:18:06 +0800

    Added capture button

--- app/src/main/java/com/liempo/furniture/MainActivity.kt
+++ app/src/main/java/com/liempo/furniture/MainActivity.kt
@@ -2,21 +2,34 @@ package com.liempo.furniture
 
 import android.app.ActivityManager
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
 import android.net.Uri
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.PixelCopy
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.google.ar.core.ArCoreApk
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var ux: CaptureArFragment
     private var model: ModelRenderable? = null
+    private val loaded =
+        arrayListOf<AnchorNode>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +44,8 @@ class MainActivity : AppCompatActivity() {
             ).show()
 
         // Get the instance of arFragment
-        val ux = supportFragmentManager
-            .findFragmentById(R.id.ux_fragment) as ArFragment
+        ux = supportFragmentManager
+            .findFragmentById(R.id.ux_fragment) as CaptureArFragment
 
         ux.setOnTapArPlaneListener { hit, _, _ ->
             if (model == null) {
@@ -48,6 +61,7 @@ class MainActivity : AppCompatActivity() {
             val anchor = hit.createAnchor()
             val node = AnchorNode(anchor).apply {
                 setParent(ux.arSceneView.scene)
+                loaded.add(this)
             }
 
             // Create the transformable node
@@ -64,6 +78,19 @@ class MainActivity : AppCompatActivity() {
         select_fab.setOnClickListener {
             dialog.show(supportFragmentManager, "Dialog")
         }
+        reset_fab.setOnClickListener {
+            loaded.forEach {
+                ux.arSceneView.scene.removeChild(it)
+                it.apply { anchor?.detach(); setParent(null) }
+            }
+
+            Toast.makeText(this,
+                "Scene has been reset",
+                Toast.LENGTH_SHORT).show()
+        }
+        capture_fab.setOnClickListener {
+            takePhoto()
+        }
     }
 
     internal fun selectModel(uri: Uri) {
@@ -73,6 +100,71 @@ class MainActivity : AppCompatActivity() {
             .thenAccept {
                 model = it
             }.exceptionally { error(it) }
+    }
+
+    private fun takePhoto() {
+        val filename = generateFilename()
+
+        val bitmap = Bitmap.createBitmap(
+            ux.arSceneView.width,
+            ux.arSceneView.height,
+            Bitmap.Config.ARGB_8888
+        )
+
+        val thread = HandlerThread("PixelCopier")
+            .apply { start() }
+
+        PixelCopy.request(ux.arSceneView, bitmap, {  result ->
+            if (result == PixelCopy.SUCCESS) {
+                saveBitmapToDisk(bitmap, filename)
+
+                // Notify gallery
+                val file = File(filename)
+                val uri = FileProvider.getUriForFile(this,
+                    "$packageName.provider", file)
+                Intent(Intent.ACTION_VIEW, uri).apply {
+                    setDataAndType(uri, "image/*")
+                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    startActivity(this)
+                }
+
+                Toast.makeText(
+                    this,
+                    "Photo saved", Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Failed to save photo", Toast.LENGTH_LONG
+                ).show()
+            }
+
+        }, Handler(thread.looper))
+
+
+    }
+
+    @Suppress("DEPRECATION")
+    private fun generateFilename(): String {
+        val format = SimpleDateFormat("yyyyMMddHHmmss",
+            Locale.getDefault())
+        val date = format.format(Date())
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).path +
+                File.separator + ALBUM_NAME + File.separator + date + SUFFIX
+    }
+
+    private fun saveBitmapToDisk(bitmap: Bitmap, filename: String) {
+        val out = File(filename)
+        if (!out.parentFile?.exists()!!)
+            out.parentFile?.mkdirs()
+
+        out.outputStream().use {
+            val data = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG,
+                100, data)
+            data.writeTo(it)
+            it.flush(); it.close()
+        }
     }
 
     private fun getOpenGLVersion(): Double =
@@ -86,5 +178,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val MIN_OPENGL_VERSION = 3.0
+        private const val ALBUM_NAME = "Furniture"
+        private const val SUFFIX = "_screenshot.jpg"
     }
 }
